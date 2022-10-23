c7e194a52129a58501641f11d29c6724e9a5179c
mleilani <mlfelipe@cpp.edu>
2022-10-23 13:51:00 -0700

    added loshu

---
+++ loshusquare/loshu.c
@@ -0,0 +1,31 @@
+//include time for rand
+
+//function to print initial square
+
+//function to fill the square with random varibales
+
+//function to check for mangic square
+
+    //store sum of first row to compare with other values
+
+    //check row 1
+
+    //check row 2
+
+    //check row 3
+
+
+    //check columns
+
+    //check diagonals
+
+
+//main function
+
+    //initialize array
+
+    //loop to check if magic square is true
+
+    // have a counter to check number of quares generated
+
+    //print grid system
