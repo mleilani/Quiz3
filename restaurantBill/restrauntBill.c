c9ad3908b6d2bf5a7d2225533bac8146108dd878
mleilani <mlfelipe@cpp.edu>
2022-10-23 12:45:45 -0700

    added total price

--- restaurantBill/restrauntBill.c
+++ restaurantBill/restrauntBill.c
@@ -11,15 +11,10 @@
 typedef struct {
     char *item;
     double price;
-}Item, *pointerToItem
+}Item, *pointerToItem;
 
 //define the food items n prices
-Item itemTable[] = {
-    {"Salad", 9.95}
-    {"Soup", 4.55}
-    {"Sandwhich", 13.25}
-    {"Pizza", 22.35}
-};
+Item itemTable[] = { {"Salad", 9.95}, {"Soup", 4.55}, {"Sandwich", 13.25}, {"Pizza", 22.35} };
 
 //create a getcost function
 double getCost(pointerToItem food, int num){
@@ -33,9 +28,24 @@ char* getName(pointerToItem food, int num){
 //main function
 int main(int argC, char *argV[]){
     if (argC != 3){
-        printf("Error, invalid arguments. First argument should be tax, the other should be tip")
+        printf("Error, invalid arguments. First argument should be tax, the other should be tip");
     }
-}
 
+    srand(time(NULL)); //to get the rand variable
+    double tax = atof(argV[1])/100;
+    double tip = atof(argV[2])/100;
+    int t = rand() % 4;
+    double price = getCost(itemTable, t);
+    double totalTax = price * tax;
+    double totalTip = price * tip;
+
+    printf("The price is %0.2f\n", price);
+    printf("The tax is %0.2f\n, totalTax");
+    printf("The tip is %0.2f\n", totalTip);
+    printf("The total bill is %0.2f\n", price + totalTax + totalTip);
+
+    return 0;
+}
 //create a error cache
+
 
