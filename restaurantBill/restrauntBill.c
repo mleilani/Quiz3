455f2fcedbc4fc20cfd1178bf583b3b7d34d9060
mleilani <mlfelipe@cpp.edu>
2022-10-23 11:46:32 -0700

    restrauntBill start

---
+++ restaurantBill/restrauntBill.c
@@ -0,0 +1,15 @@
+//include
+
+//need include time to get rand variables
+
+//create a pointer
+
+//define the food items n prices
+
+//create a getcost function
+//create a getitem function
+
+//main function
+
+//create a error cache
+
